fc34b9755d687b3e4428b0c0a83c79cc7e0206ca
owlab-io <contact@owlab.io>
2019-03-08 13:17:28 +0100

    imported user module into global store

--- j99_final_template_vue/src/store/store.js
+++ j99_final_template_vue/src/store/store.js
@@ -1,20 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import appEvents from "./appEvents"
+import users from "./users";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
-    appEvents
+    appEvents,
+    users
   }
-})
+});
