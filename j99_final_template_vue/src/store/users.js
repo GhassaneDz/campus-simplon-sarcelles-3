bcb051fee10bf479f2d51fbb24339a9be07ebe76
owlab-io <contact@owlab.io>
2019-03-08 13:16:44 +0100

    added basic users store file

---
+++ j99_final_template_vue/src/store/users.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+console.log("store user", axios);
+
+const defaultUser = {
+    id: 123,
+    name: "guillaume",
+    lastname: "amg",
+    email: "gui@ohm.com",
+};
+
+const defaultUsers = [
+    {
+        id: 13,
+        name: "toto",
+        lastname: "teteAzero",
+        email: "toto@foo.com",
+    },
+    {
+        id: 23,
+        name: "titi",
+        lastname: "parisien",
+        email: "titi@bar.com",
+    },
+    {
+        id: 66,
+        name: "tata",
+        lastname: "yoyo",
+        email: "tata@baz.com",
+    }
+];
+
+const userStore = {
+    namespaced: true,
+    state: {
+        currentUser: defaultUser,
+        allUsers: defaultUsers
+    },
+    getters: {
+        currentUser(state) {
+            return state.currentUser;
+        },
+        allUsers(state) {
+            return state.allUsers;
+        }
+    },
+    mutations: {},
+    actions: {
+        register(ctx) {
+
+        },
+        login(ctx) {
+
+        },
+        logout(ctx) {
+
+        },
+        getAllUsers(ctx) {
+
+        },
+        updateUser(ctx) {
+
+        },
+        deleteUser(ctx) {
+
+        }
+    },
+};
+
+export default userStore;
