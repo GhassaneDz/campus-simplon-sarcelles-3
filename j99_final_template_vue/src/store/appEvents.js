e5b4e6dd7307abbe62e8a4c98543095d13de3dfd
owlab-io <contact@owlab.io>
2019-03-08 10:53:36 +0100

    modif du store pour prendre en compte le component appMessage

--- j99_final_template_vue/src/store/appEvents.js
+++ j99_final_template_vue/src/store/appEvents.js
@@ -1,16 +1,23 @@
 export default {
     namespaced: true,
     state: {
+        appMessage: "default message from store",
         isNavMobileOn: false
     },
     getters: {
         navMobileStatus(state) {
             return state.isNavMobileOn
+        },
+        currentAppMessage({ appMessage }) {
+            return appMessage;
         }
     },
     mutations: {
         toggleNavMobile(state) {
             state.isNavMobileOn = !state.isNavMobileOn;
+        },
+        setCurrentMessage(state, msg) {
+            state.appMessage = msg;
         }
     }
 };
